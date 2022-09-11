4276cfeb57985bc5dd78b3f0f924f6eedf312f02
mmertylmz <dokkensavatage@gmail.com>
2022-09-11 23:53:30 +0300

    New ProductService, Create Product

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
-import { Product } from 'src/app/contracts/product';
+import { Create_Product } from 'src/app/contracts/create_product';
 
 @Component({
   selector: 'app-products',
@@ -19,56 +19,5 @@ export class ProductsComponent extends BaseComponent implements OnInit {
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallAtom);
-    this.httpClientService
-      .get<Product[]>({
-        controller: 'products',
-      })
-      .subscribe((data) => {
-        data.forEach((element) => {
-          console.log(
-            `Name: ${element.name}\nStock:${element.stock}\nPrice: ${element.price}`
-          );
-        });
-      });
-
-    // this.httpClientService
-    //   .post(
-    //     {
-    //       controller: 'products',
-    //     },
-    //     {
-    //       name: 'Kalem',
-    //       stock: 100,
-    //       price: 15,
-    //     }
-    //   )
-    //   .subscribe();
-
-    // this.httpClientService
-    //   .put(
-    //     { controller: 'products' },
-    //     {
-    //       id: '093d08ad-b85f-4eb2-88ba-16a30aa8bb0f',
-    //       name: 'Renkli Kağıt',
-    //       stock: 1500,
-    //       price: 5.5,
-    //     }
-    //   )
-    //   .subscribe();
-
-    // this.httpClientService
-    //   .delete(
-    //     {
-    //       controller: 'products',
-    //     },
-    //     'c047b8b8-4a1b-4187-91f9-cde2065dd51f'
-    //   )
-    //   .subscribe();
-
-    // this.httpClientService
-    //   .get({
-    //     fullEndPoint: 'https://jsonplaceholder.typicode.com/posts',
-    //   })
-    //   .subscribe((data) => console.log(data));
   }
 }
--- src/app/admin/components/products/products.module.ts
+++ src/app/admin/components/products/products.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+import { CreateComponent } from './create/create.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, CreateComponent, ListComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: ProductsComponent }]),
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
 })
 export class ProductsModule {}
