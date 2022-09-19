d1983793b616ca70809a493e0a88d524d5e1903d
mmertylmz <dokkensavatage@gmail.com>
2022-09-19 13:54:37 +0300

    Directive Fix & Delete Modal(Dialog) Panel

--- src/app/admin/components/products/products.module.ts
+++ src/app/admin/components/products/products.module.ts
@@ -11,6 +11,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { CreateComponent } from './create/create.component';
 import { ListComponent } from './list/list.component';
 import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DeleteDialogComponent } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
     CreateComponent,
     ListComponent,
     DeleteDirective,
+    DeleteDialogComponent,
   ],
   imports: [
     CommonModule,
@@ -28,6 +31,7 @@ import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
     MatButtonModule,
     MatTableModule,
     MatPaginatorModule,
+    MatDialogModule,
   ],
 })
 export class ProductsModule {}
--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { outputAst } from '@angular/compiler';
 import {
   Directive,
@@ -8,7 +9,19 @@ import {
   Output,
   Renderer2,
 } from '@angular/core';
-import { ProductService } from 'src/app/services/common/models/product.service';
+import { MatDialog } from '@angular/material/dialog';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from 'src/app/base/base.component';
+import {
+  DeleteDialogComponent,
+  DeleteState,
+} from 'src/app/dialogs/delete-dialog/delete-dialog.component';
+import {
+  AlertifyService,
+  MessageType,
+  Position,
+} from 'src/app/services/admin/alertify.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 declare var $: any;
 
@@ -19,7 +32,10 @@ export class DeleteDirective {
   constructor(
     private element: ElementRef,
     private _renderer: Renderer2,
-    private productService: ProductService
+    private httpClientService: HttpClientService,
+    private spinner: NgxSpinnerService,
+    public dialog: MatDialog,
+    private alertifyService: AlertifyService
   ) {
     const img = _renderer.createElement('img');
     img.setAttribute('src', '/assets/delete.png');
@@ -30,14 +46,62 @@ export class DeleteDirective {
   }
 
   @Input() id: string;
+  @Input() controller: string;
   @Output() callback: EventEmitter<any> = new EventEmitter();
 
   @HostListener('click')
   async onClick() {
-    const td: HTMLTableCellElement = this.element.nativeElement;
-    await this.productService.delete(this.id);
-    $(td.parentElement).fadeOut(1000, () => {
-      this.callback.emit();
+    this.openDialog(async () => {
+      this.spinner.show(SpinnerType.BallAtom);
+      const td: HTMLTableCellElement = this.element.nativeElement;
+      this.httpClientService
+        .delete(
+          {
+            controller: this.controller,
+          },
+          this.id
+        )
+        .subscribe(
+          (data) => {
+            $(td.parentElement).animate(
+              {
+                opacity: 0,
+                left: '+=50',
+                height: 'toggle',
+              },
+              700,
+              () => {
+                this.callback.emit();
+                this.alertifyService.message('Ürün başarıyla silinmiştir', {
+                  dismissOthers: true,
+                  messageType: MessageType.Success,
+                  position: Position.TopRight,
+                });
+              }
+            );
+          },
+          (errorResponse: HttpErrorResponse) => {
+            this.spinner.hide(SpinnerType.BallAtom);
+            this.alertifyService.message('Bir hata oluştu.', {
+              dismissOthers: true,
+              messageType: MessageType.Error,
+              position: Position.TopRight,
+            });
+          }
+        );
+    });
+  }
+
+  openDialog(afterClosed: any): void {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      width: '250px',
+      data: DeleteState.Yes,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result == DeleteState.Yes) {
+        afterClosed();
+      }
     });
   }
 }
